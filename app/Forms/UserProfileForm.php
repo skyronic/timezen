956003fa01ad8404273299f23f36dc7691ed66e7
Anirudh Sanjeev <skyronic@gmail.com>
2017-09-24 01:25:38 +0530

    updating profile

--- app/Forms/UserProfileForm.php
+++ app/Forms/UserProfileForm.php
@@ -2,6 +2,8 @@
 
 namespace App\Forms;
 
+use Carbon\Carbon;
+use DateTimeZone;
 use Kris\LaravelFormBuilder\Form;
 
 class UserProfileForm extends Form
@@ -12,6 +14,38 @@ class UserProfileForm extends Form
             ->add('name', 'text', [
                 'rules' =>'required|min:5'
             ])
+            ->timezoneSelect('timezone')
+            ->timestampSelect('day_start')
+            ->timestampSelect('ideal_start')
+            ->timestampSelect('ideal_end')
+            ->timestampSelect('day_end')
             ->add('submit', 'submit', ['label' => 'Save']);
+    }
+
+    protected function timestampSelect ($key) {
+        $choices = [];
+        for($i = 0; $i < 48; $i++ ) {
+            $ts = Carbon::create(2000, 1, 1, 0, 0, 0);
+            $ts->addMinutes($i * 30);
+            $choices [$i] = $ts->format("g:i A");
+        }
+
+        return $this->add($key, 'select', [
+            'choices' => $choices,
+            'empty_value' => 'Select'
+        ]);
+    }
+
+    protected function timezoneSelect ($key) {
+        $timezones = DateTimeZone::listIdentifiers();
+        $choices = [];
+        foreach($timezones as $tz) {
+            $choices [$tz] = $tz;
+        }
+
+        return $this->add($key, 'select', [
+            'choices' => $choices,
+            'empty_value' => 'Select'
+        ]);
     }
 }
